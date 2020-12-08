53644ddc1b51f5094a3e89d03885181689d15198
Rizwan <mohdrizwan.shaikh2112@gmail.com>
2020-12-08 12:53:09 +0530

    Updated javascript file for minute and hour hands.

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,11 +1,23 @@
-const secondsHand = document.querySelector(`.second-hand`)
+const secondsHand = document.querySelector(`.second-hand`);
+const minutesHand = document.querySelector(`.min-hand`);
+const hourHand = document.querySelector(`.hour-hand`);
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = (seconds * 6) + 90;
-    secondsHand.style.tranform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = (minutes * 6) + 90 + (seconds / 10);
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hourDegrees = (hours * 30) + 90 + (minutes / 2);
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
+
+setDate();
