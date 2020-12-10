fa2a2e2efb3f7e060a7956d62fb4b09870eff2e7
Rizwan <mohdrizwan.shaikh2112@gmail.com>
2020-12-10 19:28:38 +0530

    Improved readability

--- 04 - Array Cardio Day 1/index.js
+++ 04 - Array Cardio Day 1/index.js
@@ -88,6 +88,7 @@
     });
     console.table(alpha);
 
+    
       // 8. Reduce Exercise
       // Sum up the instances of each of these
       const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
