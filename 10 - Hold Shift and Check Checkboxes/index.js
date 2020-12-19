6d24d5deee80f687bb3dcd82796cc5df425f6b89
Rizwan <mohdrizwan.shaikh2112@gmail.com>
2020-12-19 14:31:15 +0530

    Day 10 challenge complete.

---
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastCheck;
+
+function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach((checkbox) => {
+            if (checkbox === this || checkbox === lastCheck) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    lastCheck = this;
+}
+
+checkboxes.forEach((checkbox) =>
+    checkbox.addEventListener("click", handleCheck)
+);
