44eb5e63b0ef94286ebe47b57d2a2763e47659f7
Rizwan <mohdrizwan.shaikh2112@gmail.com>
2020-12-19 18:45:03 +0530

    Day 13 challenge complete

--- 13 - Slide in on scroll/index.js
+++ 13 - Slide in on scroll/index.js
@@ -13,3 +13,22 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+    sliderImages.forEach((sliderImage) => {
+        const slideInAt =
+            window.scrollY + window.innerHeight - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrollPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrollPast) {
+            sliderImage.classList.add("active");
+        } else {
+            sliderImage.classList.remove("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
